443561bf2dfeea57bd7ddc0147508d35d661040f
sherlinvarshitha <sherlinvarshithabollam@gmail.com>
2026-05-10 15:27:48 +0530

    fix railway deployment config

--- ai-code-reviewer/backend/app.py
+++ ai-code-reviewer/backend/app.py
@@ -501,7 +501,11 @@ def analyze_github():
         return jsonify({"error": "Failed to fetch or analyze repository"}), 500
 
 
-name__ == "__main__":
+'''if__name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     #app.run(debug=True, port=port)
+    app.run(host="0.0.0.0", port=port)'''
+if __name__ == "__main__":
+    import os
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
